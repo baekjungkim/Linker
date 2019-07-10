17e0835a75224d0652e110c33ab1a273397e56c8
baekjung <zvgandam@gmail.com>
2019-07-10 14:06:54 +0900

    Webpack initialize

--- app.js
+++ app.js
@@ -12,16 +12,23 @@ const app = express();
 
 app.use(helmet());
 app.set("view engine", "pug");
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger("dev"));
 
-app.use("/dist", express.static("dist"));
-
 app.use(localMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.place, placeRouter);
+
+app.use((req, res) => {
+  res.status(400).send("일치하는 주소가 없습니다!");
+});
+app.use((err, req, res) => {
+  console.error(err.stack);
+  res.status(500).redirect(routes.home);
+});
 
 export default app;
